5cbb6d012ed1cfd2dd5a9ac17c791ba0cc5b3918
Zulqoniun <muhammadqoni123@gmail.com>
2020-10-24 11:53:31 +0700

    Add project files.

---
+++ RentalKendaraan_120/Models/Gender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_120.Models
+{
+    public partial class Gender
+    {
+        public int IdGender { get; set; }
+        public string NamaGender { get; set; }
+    }
+}
---
+++ RentalKendaraan_120/Models/Rental_KendaraanContext.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace RentalKendaraan_120.Models
+{
+    public partial class Rental_KendaraanContext : DbContext
+    {
+        public Rental_KendaraanContext()
+        {
+        }
+
+        public Rental_KendaraanContext(DbContextOptions<Rental_KendaraanContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<Gender> Gender { get; set; }
+        public virtual DbSet<Jaminan> Jaminan { get; set; }
+        public virtual DbSet<JenisKendaraan> JenisKendaraan { get; set; }
+        public virtual DbSet<Kendaraan> Kendaraan { get; set; }
+        public virtual DbSet<Peminjaman> Peminjaman { get; set; }
+        public virtual DbSet<Pengembalian> Pengembalian { get; set; }
+
+        // Unable to generate entity type for table 'dbo.Kondisi Kendaraan'. Please see the warning messages.
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(e => e.IdCustomer);
+
+                entity.Property(e => e.IdCustomer)
+                    .HasColumnName("ID_Customer")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Alamat)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IdGender).HasColumnName("ID_Gender");
+
+                entity.Property(e => e.NamaCustomer)
+                    .HasColumnName("Nama_Customer")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nik)
+                    .HasColumnName("NIK")
+                    .HasMaxLength(16)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoHp)
+                    .HasColumnName("No_HP")
+                    .HasMaxLength(13)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Gender>(entity =>
+            {
+                entity.HasKey(e => e.IdGender);
+
+                entity.Property(e => e.IdGender)
+                    .HasColumnName("ID_Gender")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.NamaGender)
+                    .HasColumnName("Nama_Gender")
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Jaminan>(entity =>
+            {
+                entity.HasKey(e => e.IdJaminan);
+
+                entity.Property(e => e.IdJaminan)
+                    .HasColumnName("ID_Jaminan")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.NamaJaminan)
+                    .HasColumnName("Nama_Jaminan")
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<JenisKendaraan>(entity =>
+            {
+                entity.HasKey(e => e.IdJenisKendaraan);
+
+                entity.ToTable("Jenis Kendaraan");
+
+                entity.Property(e => e.IdJenisKendaraan)
+                    .HasColumnName("ID_Jenis_Kendaraan")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.NamaJenisKendaraan)
+                    .HasColumnName("Nama_Jenis_Kendaraan")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Kendaraan>(entity =>
+            {
+                entity.HasKey(e => e.IdKendaraan);
+
+                entity.Property(e => e.IdKendaraan)
+                    .HasColumnName("ID_Kendaraan")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.IdJenisKendaraan).HasColumnName("ID_Jenis_Kendaraan");
+
+                entity.Property(e => e.Ketersediaan)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NamaKendaraan)
+                    .HasColumnName("Nama_Kendaraan")
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoPolisi)
+                    .HasColumnName("No_Polisi")
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoStnk)
+                    .HasColumnName("No_STNK")
+                    .HasMaxLength(8)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Peminjaman>(entity =>
+            {
+                entity.HasKey(e => e.IdPeminjaman);
+
+                entity.Property(e => e.IdPeminjaman)
+                    .HasColumnName("ID_Peminjaman")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.IdCustomer).HasColumnName("ID_Customer");
+
+                entity.Property(e => e.IdJaminan).HasColumnName("ID_Jaminan");
+
+                entity.Property(e => e.IdKendaraan).HasColumnName("ID_Kendaraan");
+
+                entity.Property(e => e.TglPeminjaman)
+                    .HasColumnName("Tgl_Peminjaman")
+                    .HasColumnType("datetime");
+            });
+
+            modelBuilder.Entity<Pengembalian>(entity =>
+            {
+                entity.HasKey(e => e.IdPengembalian);
+
+                entity.Property(e => e.IdPengembalian)
+                    .HasColumnName("ID_Pengembalian")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.IdKondisi).HasColumnName("ID_Kondisi");
+
+                entity.Property(e => e.IdPeminjaman).HasColumnName("ID_Peminjaman");
+
+                entity.Property(e => e.TglPengembalian)
+                    .HasColumnName("Tgl_Pengembalian")
+                    .HasColumnType("datetime");
+            });
+        }
+    }
+}
